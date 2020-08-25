8a1bb90fd2a37d012cbaff1f4c310549f1da6c88
Stefan Mikic <stefan.mikic@towa.at>
2020-08-25 19:30:39 +0200

    feat: implemented first plugin

---
+++ markdown-parser.js
@@ -0,0 +1,27 @@
+class MarkdownParser
+{
+  constructor({ data, api }) {
+    this.data = data;
+    this.api = api;
+  }
+  static get toolbox() {
+    return {
+      title: 'Markdown Parser',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="17" height="15" viewBox="0 0 24 24" fill="none" stroke="#707684" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-save"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg>'
+    };
+  }
+
+  render()
+  {
+    console.log('Data log');
+    console.log(this.data);
+
+  }
+
+  save()
+  {
+    return {
+      message: 'Parsing Markdown'
+    }
+  }
+}
